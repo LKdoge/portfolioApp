c1e10a3874b8be7f523b80ddb1c8bcdd8fc48ef3
Lemitssu <maicolcampos47@gmail.com>
2025-03-27 09:15:17 -0500

    feacture: herouicli integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Page from "../config/db-conection";
+import {Button} from '@heroui/button'; 
 export default function Home() {
   return (
     <div className="">
-      <h1>Hello World</h1>
       <Page />
+    <div>
+      <Button isDisabled color="primary">
+          Button
+      </Button>
+    </div>
     </div>
   );
 }
