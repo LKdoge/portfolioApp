4e176af5566ad5101715210edf75fb55fe260e18
Santiago <santi985054@hotmail.com>
2025-04-12 13:20:00 -0500

    Modify width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,17 @@
 
 export default function HomePage() {
   return (
-    <div className="w-[90%] m-auto ">
-        <div className="grid gap-x-4 gap-y-3 grid-flow-row-dense">
-            <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]" />
-            <div className="bg-orange-500 rounded-lg shadow-xl min-h-[50px]" />
-            <div className="bg-blue-500 rounded-lg shadow-xl min-h-[50px]" />
-            <div className="bg-rose-800 rounded-lg shadow-xl min-h-[50px] col-span-2" />
-            <div className="bg-yellow-500 rounded-lg shadow-xl min-h-[50px]" />
-            <div className="bg-pink-500 rounded-lg shadow-xl min-h-[50px]" />
-            <div className="bg-amber-100 rounded-lg shadow-xl min-h-[50px]" />
-            <div className="bg-purple-500 rounded-lg shadow-xl min-h-[50px]" />
-            <div className="bg-lime-200 rounded-lg shadow-xl min-h-[50px]" />
-            <div className="bg-indigo-500 rounded-lg shadow-xl min-h-[50px]" />
+    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+      <div className="grid grid-cols-4 gap-x-4 gap-y-5 grid-flow-row-dense w-full max-w-6xl">
+        <div className="bg-red-500 rounded-lg shadow-xl min-h-[500px] col-span-4 row-span-3" />
+        <div className="bg-orange-500 rounded-lg shadow-xl min-h-[300px]" />
+        <div className="bg-blue-500 rounded-lg shadow-xl min-h-[300px]" />
+        <div className="bg-rose-800 rounded-lg shadow-xl min-h-[300px]" />
+        <div className="bg-yellow-500 rounded-lg shadow-xl min-h-[300px] col-span-4" />
+        <div className="bg-pink-500 rounded-lg shadow-xl min-h-[300px]" />
+        <div className="bg-amber-100 rounded-lg shadow-xl min-h-[300px] col-span-2" />
+        <div className="bg-purple-500 rounded-lg shadow-xl min-h-[300px] col-span-2" />
+        <div className="bg-lime-200 rounded-lg shadow-xl min-h-[200px] col-span-4" />
       </div>
     </ div>
   );
